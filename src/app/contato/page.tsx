8576aa2a02960bd836fd8d902230b2cbadfd3586
lara.capila <lara.capila@way2.com.br>
2023-06-07 23:28:10 -0300

    feat: Navigation menu

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Headdings from '../components/Typography/Headings';
+
+function Contact() {
+  return (
+    <Headdings text="Contato" size="text-4xl" tag="h1" />
+  );
+}
+
+export default Contact;
